8af13fc79e1c293819f168c783088a1ef2f06d71
Ayoosa <130565305+Ayoosa@users.noreply.github.com>
2026-08-02 15:42:33 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // 将 Supabase 项目设置中的 URL 和 anon public key 填到这里。
 // anon key 可公开放在 GitHub Pages；不要填写 service_role key。
 window.SUPABASE_CONFIG = {
-  url: 'https://dorquqwhgmglshylznug.supabase.co',
+  url: 'https://dorquqwhgmglshylznug.supabase.co/rest/v1/',
   anonKey: 'sb_publishable_CczHMwSfF3wekUP6giPAPQ_q5hxmUU3'
 };
