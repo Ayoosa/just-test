38fbd7bba899d83663d5679eb158213e93694eb9
Ayoosa <130565305+Ayoosa@users.noreply.github.com>
2026-08-02 16:34:53 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // 将 Supabase 项目设置中的 URL 和 anon public key 填到这里。
 // anon key 可公开放在 GitHub Pages；不要填写 service_role key。
 window.SUPABASE_CONFIG = {
-  url: 'https://dorquqwhgmglshylznug.supabase.co',
+  url: 'https://dorquqwhgmglshylznug.supabase.co/rest/v1/',
   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRvcnF1cXdoZ21nbHNoeWx6bnVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU2NDkzMjUsImV4cCI6MjEwMTIyNTMyNX0.bdxBA66KlMf6o2p4-Wx1xakkqwt_B00qgRYwIaIHtPM'
 };
